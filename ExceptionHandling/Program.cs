de3057e36817719cb25fc70f7630ba420cafe699
P Dring <pddring@googlemail.com>
2020-11-04 14:48:19 +0000

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Challenges:
+             * 1a) Create a program which asks the user to type their age.
+             *  It shouldn't crash, no matter what they type in
+             *  
+             * 1b) Create a program which keeps asking the user to type in their age
+             * until they type in something between 0 and 150
+             * 
+             * 2a) Create a program which asks the user to type in a 16 digit credit card number
+             *  It shouldn't crash, no matter what they type in
+             *  
+             * 2b) Create a program which keeps asking the user to type in a 16 digit card number
+             * until they enter something valid
+             * 
+             * Don't use real credit card numbers
+             * */
+        }
+    }
+}
